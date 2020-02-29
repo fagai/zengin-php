6cc79675455dc8ac112ac237e3b8ed97a425478b
fagai <fagai@fagai.net>
2020-03-01 03:14:12 +0900

    change clone code

--- src/ZenginCode.php
+++ src/ZenginCode.php
@@ -4,7 +4,7 @@ namespace Fagai\ZenginCode;
 
 class ZenginCode
 {
-    public const DATA_PATH = __DIR__ . '/../zengin/data';
+    public const DATA_PATH = __DIR__ . '/../vendor/zengin-code/source-data/data';
 
     /**
      * @param $code
